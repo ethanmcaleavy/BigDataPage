9dc336f5540a7e2a3bffb71d2c933b8f41651aaa
StevenD6378 <stda6378@colorado.edu>
2023-04-20 22:51:40 -0600

    changed urls of website and added default image

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ server.use('/upload', express.static('upload'));
 server.use(fileUpload());
 server.set('view engine', 'ejs');
 
+let fileName = "error";
 
 server.listen(8080, () => {
   console.log("Application started and Listening on port 8080");
@@ -18,10 +19,9 @@ server.get('/', (req, res) => {
     res.render('helloworld.ejs');
   });
 
-  server.get('/newpage/:fileNameB', function(req, res) {
-    const fileNameA = req.params.fileNameB;
-    console.log("second " + fileNameA)
-    res.render('secondHelloWorld.ejs',  { name: fileNameA});
+  server.get('/newpage', function(req, res) {
+    console.log("second " + fileName)
+    res.render('secondHelloWorld.ejs',  { name: fileName});
 });
 
 server.get('/newpage2', function(req, res) {
@@ -31,14 +31,13 @@ server.get('/newpage2', function(req, res) {
 server.post('/upload', (req, res) => {
   // Get the file that was set to our field named "image"
   const { image } = req.files;
-  const fileNameB = image.name;
-  console.log("first " + fileNameB)
+  fileName = image.name;
+  console.log("image name: " + fileName)
 
   // If no image submitted, exit
   if (!image) return res.sendStatus(400);
 
   // Move the uploaded image to our upload folder
   image.mv(__dirname + '/upload/' + image.name);
-  res.redirect('/newpage/' + encodeURIComponent(fileNameB));
-
+  res.redirect('/newpage');
 });
