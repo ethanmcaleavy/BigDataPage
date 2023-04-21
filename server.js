1ef3bd9e92f8354baa77fbdf32e3ef4973eabaa2
ethanmcaleavy <ethan.mcaleavy@gmail.com>
2023-04-20 20:43:10 -0600

    Added image uplaod using express upload

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const server = express();
+const fileUpload = require('express-fileupload');
 
 server.use(express.static('public'));
 server.use('/images', express.static('images'));
+server.use(fileUpload());
+
+
 
 server.listen(8080, () => {
   console.log("Application started and Listening on port 8080");
@@ -18,4 +22,17 @@ server.get('/', (req, res) => {
 
 server.get('/newpage2', function(req, res) {
     res.sendFile(__dirname + '/helloworld.html');
+});
+
+server.post('/upload', (req, res) => {
+  // Get the file that was set to our field named "image"
+  const { image } = req.files;
+
+  // If no image submitted, exit
+  if (!image) return res.sendStatus(400);
+
+  // Move the uploaded image to our upload folder
+  image.mv(__dirname + '/upload/' + image.name);
+
+  res.sendStatus(200);
 });
