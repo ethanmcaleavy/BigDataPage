505174e129d806d6b81ec59a6d39ed6bc3c8b586
StevenD6378 <stda6378@colorado.edu>
2023-04-26 18:29:32 -0600

    adding correct pages2

--- pickleNN.py
+++ pickleNN.py
@@ -8,44 +8,44 @@ import cv2
 from deepface import DeepFace
 from deepface.commons import distance
 
-def main(uploadedImage):
-    print(uploadedImage)
 
-    mat = scipy.io.loadmat('C:/Users/steve/Downloads/imageData/imdb_crop/imdb')
+def main(uploadedImage):
+
+
+    mat = scipy.io.loadmat('./imageData/imdb_crop/imdb')
     instances = mat['imdb'][0][0][0].shape[1]
-    df = pd.read_pickle('C:/Users/steve/BigDataPage/FacialNNDFtest3.pkl')
+    df = pd.read_pickle('./FacialNNDFtest3.pkl')
     testfacefilepath = uploadedImage #'photo.jpg'
-    img = cv2.imread('C:/Users/steve/downloads/'+testfacefilepath)
-    # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    initial_representation = DeepFace.represent(img_path = 'C:/Users/steve/downloads/'+testfacefilepath,model_name="VGG-Face", detector_backend="opencv")
+    img = cv2.imread('./upload/'+testfacefilepath)
+    initial_representation = DeepFace.represent(img_path = './upload/'+testfacefilepath,model_name="VGG-Face", detector_backend="opencv", enforce_detection = False)
     yourself_representation = initial_representation[0]["embedding"]
     from deepface.commons import distance
     distance1 = []
 
+
     for index in df.index:
         distance1.append(distance.findCosineDistance(df['face_vector_raw'][index],yourself_representation))
+
 
     df['similarity']=distance1
     df = df.sort_values(by=['similarity'], ascending=True)
     pivot_df = df.drop_duplicates(subset="celebrity_name")
     pivot_df = pivot_df[pivot_df['face_score']>=3.5]
 
+
     arr = [] #initialize empty array to return
 
-    for i in range(0, 5): #6
+
+    for i in range(0, 4): #6
         instance = pivot_df.iloc[i]
         name = instance['celebrity_name']
         similarity = instance['similarity']
-        
         similarity = (1 - similarity)*100
-        
-        #img = instance['pixels']
         full_path = instance['full_path'][0]
-        img = cv2.imread("C:/Users/steve/Downloads/imageData/imdb_crop/%s" % full_path)
-        
-        tempArr = [full_path, name, similarity]
+        img = cv2.imread("./imageData/imdb_crop/%s" % full_path)
+       
+        tempArr = [full_path, name, round(similarity, 2)]
         arr.append(tempArr)
-        # print(full_path, name, similarity)
     print(json.dumps(arr))
 
 
--- server.js
+++ server.js
@@ -3,52 +3,42 @@ const server = express();
 const fileUpload = require('express-fileupload');
 const { spawn } = require("child_process");
 
+
 server.use(express.static(__dirname + '/'));
 server.use('/upload', express.static('upload'));
 server.use(fileUpload());
 server.set('view engine', 'ejs');
 
+
 const func = () => { //code in func to remove global variables
+
 
   let fileName = "error.png";
   let uploadsArr = []; //array of returned celebrity data
   let fileArr = []; //array of uploaded images
 
+
   server.listen(8080, () => {
     console.log("Application started and Listening on port 8080");
   });
+
 
   server.get('/', (req, res) => {
     res.render('pages/home.ejs');
   });
 
+
   server.get('/uploads', function(req, res) {
     if (fileName == "error.png") //Only allow uploads page if user has inputted valid image
       return res.redirect('/');
-
-      //Create child process
-      const py = spawn('python',['pickleNN.py', fileName]);
-      console.log("Entered server.get with fileName: ", fileName);
-      //Execute python file
-      py.stdout.on(`data`, (data) =>{
-        console.log(`stdout: ${data}`);
-        console.log(JSON.stringify(data));
-        console.log(JSON.parse(JSON.stringify(data)))
-
-        uploadsArr.push(JSON.parse(data.toString()));
-        fileArr.push(fileName);
-        res.render('pages/uploads.ejs', { name: fileArr, data: uploadsArr });
-      });
- 
-      py.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
-
+    res.render('pages/uploads.ejs', { name: fileArr, data: uploadsArr });
   });
+
 
   server.get('/about', function(req, res) {
     res.render('pages/about.ejs');
   });
+
 
   server.post('/uploads', (req, res) => {
     // Get the file that was set to our field named "image"
@@ -56,18 +46,39 @@ const func = () => { //code in func to remove global variables
     fileName = image.name;
     console.log("image name in post uploads: " + fileName)
 
+
     // If no image submitted, exit
     if (!image) return res.sendStatus(400);
+
 
     // Move the uploaded image to our upload folder
     image.mv(__dirname + '/upload/' + image.name);
 
+
     if (fileName == "error.png") //Only allow uploads page if user has inputted valid image
       return res.redirect('/');
-    else
-      res.redirect('/uploads');
    
+    //Create child process
+    const py = spawn('python3',['pickleNN.py', fileName]);
+    console.log("Entered server.get with fileName: ", fileName);
+    //Execute python file
+    py.stdout.on(`data`, (data) =>{
+      console.log(`stdout: ${data}`);
+      console.log(JSON.stringify(data));
+      console.log(JSON.parse(JSON.stringify(data)))
+
+
+      uploadsArr.push(JSON.parse(data.toString()));
+      fileArr.push(fileName);
+      res.redirect('/uploads');
+    });
+
+
+    py.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
   });
 }
+
 
 func();
